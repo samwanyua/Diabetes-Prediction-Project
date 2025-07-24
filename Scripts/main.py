a29f94d678d4c98c5d39289c198a6a8bd9d45111
samwanyua <samexample8@gmail.com>
2025-07-24 14:09:39 +0300

    load saved  model

---
+++ Scripts/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pickle
+
+loaded_model = pickle.load(open('./Notebooks/trained_model.sav', 'rb'))
+
+# make a predictive system
+input_data = (1,85,66,29,0,26.6,0.351,31)
+# change the input data to numpy array
+input_data_as_np_array= np.asarray(input_data)
+# reshape the array
+input_data_reshape = input_data_as_np_array.reshape(1,-1)
+print(input_data_reshape)
+prediction = loaded_model.predict(input_data_reshape)
+print(prediction)
+
+if (prediction[0] == 0):
+    print('The person is not diabetic')
+else:
+    print('The person is diabetic.')
